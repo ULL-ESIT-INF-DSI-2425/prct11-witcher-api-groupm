fabdb3188e1d5708bff34d960f383450a82a8655
Daniel Enrique Gomez Alcala <alu0101547961@ull.edu.es>
2025-04-26 16:34:21 +0000

    Iniciando el repo

---
+++ eslint.config.mjs
@@ -0,0 +1,29 @@
+import globals from "globals";
+import pluginJs from "@eslint/js";
+import tseslint from "typescript-eslint";
+import tsdoc from "eslint-plugin-tsdoc";
+
+/** @type {import('eslint').Linter.Config[]} */
+export default [
+  { files: ["**/*.{js,mjs,cjs,ts}"] },
+  { languageOptions: { globals: globals.node } },
+  pluginJs.configs.recommended,
+  ...tseslint.configs.recommended,
+  {
+    plugins: {
+      tsdoc
+    },
+  },
+  {
+    rules: {
+      "tsdoc/syntax": "warn",
+      "prefer-const": "off",
+    }
+  },
+  {
+    ignores: [
+      "eslint.config.mjs", 
+      "dist/*", 
+      "docs/*"]
+  }
+];
