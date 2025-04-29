bb3a2316b6232e140fa14ff0db27c1d87719ba37
Jose Angel <alu0101564180@ull.edu.es>
2025-04-29 11:56:34 +0000

    Configurando render

---
+++ src/app.ts
@@ -0,0 +1,17 @@
+import express from 'express';
+
+const app = express();
+const port = process.env.PORT || 3000; // Usa el puerto de la variable de entorno o 3000 como predeterminado
+
+// Middleware to parse JSON
+app.use(express.json());
+
+// Basic route
+app.get('/', (_, res) => {
+  res.send('Servidor Express funcionando!');
+});
+
+// Start the server
+app.listen(port, () => {
+  console.log(`Servidor rodando em http://localhost:${port}`);
+});
